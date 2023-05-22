a0d41a23fe9a1b5a48604c163f6cca7dabceeed4
jerxy <jerxylol@gmail.com>
2023-05-22 02:33:31 +0200

    readability key values patch

--- gui.py
+++ gui.py
@@ -2,9 +2,9 @@ import functions
 import PySimpleGUI as Gui
 
 label = Gui.Text("Type in a to-do")
-input_box = Gui.InputText(tooltip="Enter todo", key="todo")
+input_box = Gui.InputText(tooltip="Enter todo", key="enter_todo")
 add_button = Gui.Button("Add")
-list_box = Gui.Listbox(values=functions.get_todos(), key='todos',
+list_box = Gui.Listbox(values=functions.get_todos(), key='todos_list',
                        enable_events=True, size=(45, 10))
 edit_button = Gui.Button("Edit")
 
@@ -18,24 +18,24 @@ while True:
     button_pressed, values = window.read()
     print(1, button_pressed)
     print(2, values)
-    print(3, values['todos'])
+    print(3, values['todos_list'])
     match button_pressed:
         case "Add":
             todos = functions.get_todos()
-            todos.append(values["todo"] + "\n")
+            todos.append(values["enter_todo"] + "\n")
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            window['todos_list'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            todo_to_edit = values['todos_list'][0]
+            new_todo = values['enter_todo']
 
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo + '\n'
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            window['todos_list'].update(values=todos)
+        case 'todos_list':
+            window['enter_todo'].update(value=values['todos_list'][0])
         case Gui.WINDOW_CLOSED:
             break
 
