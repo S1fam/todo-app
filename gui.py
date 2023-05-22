418c7e5f40e260c1c21930774becb0148948c771
jerxy <jerxylol@gmail.com>
2023-05-23 00:22:03 +0200

    Change theme, add clock and error handling

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import PySimpleGUI as Gui
+import time
 
+Gui.theme("DarkPurple4")
+
+clock = Gui.Text('', key='clock')
 label = Gui.Text("Type in a to-do")
 input_box = Gui.InputText(tooltip="Enter todo", key="enter_todo")
 add_button = Gui.Button("Add")
@@ -13,41 +17,46 @@ exit_button = Gui.Button("Exit")
 
 
 window = Gui.Window('My To-Do App',
-                    layout=[[label], [input_box, add_button],  # ...........79
+                    layout=[[clock],
+                            [label], [input_box, add_button],  # ...........79
                             [list_box, edit_button, complete_button],
                             [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
-    button_pressed, values = window.read()
-    print(1, button_pressed)
-    print(2, values)
-    print(3, values['todos_list'])
+    button_pressed, values = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%d.%m.%Y - %H:%M:%S"))
     match button_pressed:
         case "Add":
             todos = functions.get_todos()
-            todos.append(values["enter_todo"] + "\n")
+            new_todo = values['enter_todo'] + '\n'
+            todos.append(new_todo)
             functions.write_todos(todos)
             window['todos_list'].update(values=todos)
-            window['enter_todo'].update(value="")
 
         case "Edit":
-            todo_to_edit = values['todos_list'][0]
-            new_todo = values['enter_todo']
+            try:
+                todo_to_edit = values['todos_list'][0]
+                new_todo = values['enter_todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            functions.write_todos(todos)
-            window['todos_list'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos_list'].update(values=todos)
+            except IndexError:
+                Gui.popup("Please select an item", font=("Helvetica", 20))
 
         case "Complete":
-            todo_to_complete = values['todos_list'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos_list'].update(values=todos)
-            window['enter_todo'].update(value="")
+            try:
+                todo_to_complete = values['todos_list'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos_list'].update(values=todos)
+                window['enter_todo'].update(value="")
+            except IndexError:
+                Gui.popup("You have virus in your computer", font=("Helvetica", 20))
 
         case "Exit":
             break
