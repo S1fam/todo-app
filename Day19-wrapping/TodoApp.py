30c1a274a98fdffe6166a54f5de10c4b26a5cf7e
jerxy <jerxylol@gmail.com>
2023-05-23 22:09:13 +0200

    executable file, moving folders

---
+++ Day19-wrapping/TodoApp.py
@@ -0,0 +1,81 @@
+import functions
+import PySimpleGUI as Gui
+import time
+import os
+
+directory = "C:/JerxyhoProgramFiles"
+os.makedirs(directory, exist_ok=True)
+
+if not os.path.exists("C:/JerxyhoProgramFiles/todos.txt"):
+    with open("C:/JerxyhoProgramFiles/todos.txt", "w") as file:
+        pass
+
+Gui.theme("DarkPurple4")
+
+clock = Gui.Text('', key='clock')
+label = Gui.Text("Type in a to-do")
+input_box = Gui.InputText(tooltip="Enter todo", key="enter_todo")
+add_button = Gui.Button("Add")
+list_box = Gui.Listbox(tooltip="List of todos", values=functions.get_todos("C:/JerxyhoProgramFiles/todos.txt"),
+                       key='todos_list', enable_events=True, size=(45, 10))
+
+edit_button = Gui.Button("Edit")
+complete_button = Gui.Button("Complete")
+exit_button = Gui.Button("Exit")
+
+
+window = Gui.Window('My To-Do App',
+                    layout=[[clock],
+                            [label], [input_box, add_button],  # ...........79
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
+                    font=('Helvetica', 12))
+
+while True:
+    button_pressed, values = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%d.%m.%Y - %H:%M:%S"))
+    match button_pressed:
+        case "Add":
+            todos = functions.get_todos("C:/JerxyhoProgramFiles/todos.txt")
+            new_todo = values['enter_todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos, "C:/JerxyhoProgramFiles/todos.txt")
+            window['todos_list'].update(values=todos)
+
+        case "Edit":
+            try:
+                todo_to_edit = values['todos_list'][0]
+                new_todo = values['enter_todo']
+
+                todos = functions.get_todos("C:/JerxyhoProgramFiles/todos.txt")
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos, "C:/JerxyhoProgramFiles/todos.txt")
+                window['todos_list'].update(values=todos)
+            except IndexError:
+                Gui.popup("Please select an item", font=("Helvetica", 20))
+
+        case "Complete":
+            try:
+                todo_to_complete = values['todos_list'][0]
+                todos = functions.get_todos("C:/JerxyhoProgramFiles/todos.txt")
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos, "C:/JerxyhoProgramFiles/todos.txt")
+                window['todos_list'].update(values=todos)
+                window['enter_todo'].update(value="")
+            except IndexError:
+                Gui.popup("You have virus in your computer", font=("Helvetica", 20))
+
+        case "Exit":
+            break
+
+        case 'todos_list':
+            try:
+                window['enter_todo'].update(value=values['todos_list'][0])
+            except IndexError:
+                Gui.popup("Neco tam pridej :)")
+
+        case Gui.WINDOW_CLOSED:
+            break
+
+window.close()
---
+++ Day19-wrapping/cli-todolist.py
@@ -0,0 +1,85 @@
+# Day14: we put functions into module and connect the ModuleTodoFunctions
+# Day15: introduction to git, standard modules
+
+from ModuleTodoFunctions.functions import get_todos, write_todos, show_todos
+import time
+
+now = time.strftime("%d.%m.%Y - %H:%M:%S")
+print("It is:", now)
+while True:
+    # get user input and strip space characters from it. Input decides an action we take
+    user_action = input("Type add, complete, show, edit or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):  # if the 'add' is first in the inputed string
+        todo = user_action[4::]
+
+        todos = get_todos('../files/todos.txt')  # we have specified file as default parameter
+
+        todos.append(todo + '\n')  # appending list with new action
+
+        write_todos(filepath='../files/todos.txt', todos_argument=todos)  # this way we don't have to respect args order
+
+    elif user_action.startswith('complete'):
+        try:
+            try:
+                todos = get_todos('../files/todos.txt')
+
+                number = int(user_action[9:])
+                index = number - 1
+
+                todo_to_remove = todos[index].strip('\n')
+                todos.pop(index)
+                print(f"Todo \"{todo_to_remove}\" was removed from the list, new list:")
+
+                show_todos(todos)
+
+                write_todos(todos, '../files/todos.txt')
+
+            except IndexError:
+                print("There is no item with that number")
+                continue
+        except ValueError:
+            print("Wrong value (none or characters)")
+            continue
+
+    elif user_action.startswith('show'):
+        todos = get_todos('../files/todos.txt')
+
+        show_todos(todos)
+
+    elif user_action.startswith('edit'):
+        try:  # we try this part of code, if we get value error (user inputs letters instead of numbers) we go except
+            todos = get_todos('../files/todos.txt')
+
+            show_todos(todos)
+
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+
+            counter = 0
+            for item in todos:
+                counter += 1
+
+            if 0 <= number < counter:
+                print(f"editing: {todos[number]}")
+                new_todo = input("Enter new todo: ")
+                todos[number] = new_todo + "\n"
+
+                show_todos(todos)
+
+                write_todos(todos, '../files/todos.txt')
+
+            else:
+                print("\ninvalid number of item\n")
+        except ValueError:
+            print("your command is not valid")
+            continue  # goes back to beginning of the while loop
+
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("try working command")
+
+print("Bye!")
